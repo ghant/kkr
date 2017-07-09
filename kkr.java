58669aed34fcc688f9750d59eb79f98c8a86464d
ghant <kranthi_maddiletireddy@srmuniv.edu.in>
2017-07-09 12:09:21 +0530

    Add files via upload

---
+++ kkr.java
@@ -0,0 +1,23 @@
+class MyProgram
+{
+public static void main(String args[])
+{
+int n=5;
+if(n==1)
+System.out.println('monday');
+else if(n==2)
+System.out.println('tuesday');
+else if(n==3)
+System.out.prinjava 17 & 16tln('wednesday');
+else if(n==4)
+System.out.println('thursday');
+else if(n==5)
+System.out.println('friday');
+else if(n==6)
+System.out.println('saturday');
+else if(n==7)
+System.out.println('sunday');
+else
+System.out.println('wrong day');
+}
+}
